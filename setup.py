2aedc7983bba92ec66ba18396503fa3428918744
Chris McDonough <chrism@plope.com>
2011-02-25 20:26:26 -0500

    moved from svn; remove ez_setup

--- setup.py
+++ setup.py
@@ -16,14 +16,15 @@ __version__ = '0.4-dev'
 
 import os
 
-from ez_setup import use_setuptools
-use_setuptools()
-
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.txt')).read()
-CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+try:
+    README = open(os.path.join(here, 'README.txt')).read()
+    CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+except:
+    README = ''
+    CHANGES = ''
 
 setup(name='repoze.mailin',
       version=__version__,
